629f819fb19168c9deac268c9bbc5d9d2a574965
EricHu511 <zhu927@aucklanduni.ac.nz>
2015-07-30 22:29:30 +1200

    PCM done

--- app/src/main/java/edu/cmu/pocketsphinx/demo/ServerActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/ServerActivity.java
@@ -1,17 +1,27 @@
 package edu.cmu.pocketsphinx.demo;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-import edu.cmu.pocketsphinx.demo.R;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ServerActivity extends Activity {
     private Button record;
+    boolean recording;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +34,29 @@ public class ServerActivity extends Activity {
                 {
                     case MotionEvent.ACTION_DOWN:
                     {
-
+                    System.out.println("DOWN DOWN DOWN DOWN DOWN ");
                         // press
+                        Thread recordThread = new Thread(new Runnable(){
+
+                            @Override
+                            public void run() {
+                                recording = true;
+                                startRecord();
+                            }
+
+                        });
+
+                        recordThread.start();
+
 
 
                         return true;
                     }
                     case MotionEvent.ACTION_UP:
                     {
-
+                        System.out.println("UP UP UP UP UP ");
                         // release
-
+                        recording = false;
 
                         return true;
                     }
@@ -67,5 +89,46 @@ public class ServerActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startRecord(){
+
+        File file = new File(Environment.getExternalStorageDirectory(), "test.pcm");
+
+        try {
+            file.createNewFile();
+
+            OutputStream outputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+
+            int minBufferSize = AudioRecord.getMinBufferSize(16000,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+
+            short[] audioData = new short[minBufferSize];
+
+            AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    16000,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minBufferSize);
+
+            audioRecord.startRecording();
+
+            while(recording){
+                int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
+                for(int i = 0; i < numberOfShort; i++){
+                    dataOutputStream.writeShort(audioData[i]);
+                }
+            }
+
+            audioRecord.stop();
+            dataOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
