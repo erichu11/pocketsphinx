b3f4617a590b0f2c95a908369dba1d9aaf0797ac
EricHu511 <zhu927@aucklanduni.ac.nz>
2015-08-07 15:39:37 +1200

    finished server frontend
    also added effects when hold button

--- app/src/main/java/edu/cmu/pocketsphinx/demo/ServerActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/ServerActivity.java
@@ -1,42 +1,60 @@
 package edu.cmu.pocketsphinx.demo;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class ServerActivity extends Activity {
     private Button record;
-    boolean recording;
+    boolean recording=false;
+
+    RecordAudio recordTask;
+    PlayAudio playTask;
+    File recordingFile;
+
+    int frequency = 16000,channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
         record = (Button) this.findViewById(R.id.button_record);
-        record.setOnTouchListener( new View.OnTouchListener() {
+        record.setBackgroundColor(Color.GRAY);
+        record.setOnTouchListener(new View.OnTouchListener() {
 
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction())
-                {
-                    case MotionEvent.ACTION_DOWN:
-                    {
-                    System.out.println("DOWN DOWN DOWN DOWN DOWN ");
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        record.setBackgroundColor(Color.LTGRAY);
+                        record.setText("Release to end");
                         // press
-                        Thread recordThread = new Thread(new Runnable(){
+                        Thread recordThread = new Thread(new Runnable() {
 
                             @Override
                             public void run() {
@@ -49,14 +67,16 @@ public class ServerActivity extends Activity {
                         recordThread.start();
 
 
-
                         return true;
                     }
-                    case MotionEvent.ACTION_UP:
-                    {
-                        System.out.println("UP UP UP UP UP ");
+                    case MotionEvent.ACTION_UP: {
+                        record.setBackgroundColor(Color.GRAY);
+                        record.setText("Hold to record");
                         // release
                         recording = false;
+
+                        playTask = new PlayAudio();
+                        playTask.execute();
 
                         return false;
                     }
@@ -65,6 +85,16 @@ public class ServerActivity extends Activity {
                 }
             }
         });
+
+        File path = new File(
+                Environment.getExternalStorageDirectory().getAbsolutePath()
+                        + "/Sphinx/");
+        path.mkdirs();
+        try {
+            recordingFile = File.createTempFile("recording", ".pcm", path);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't create file on SD card", e);
+        }
 
     }
 
@@ -93,42 +123,80 @@ public class ServerActivity extends Activity {
 
     private void startRecord(){
 
-        File file = new File(Environment.getExternalStorageDirectory(), "test.pcm");
+        recordTask = new RecordAudio();
+        recordTask.execute();
 
-        try {
-            file.createNewFile();
+    }
 
-            OutputStream outputStream = new FileOutputStream(file);
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+    private class RecordAudio extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            recording = true;
+            try {
+                DataOutputStream dos = new DataOutputStream(
+                        new BufferedOutputStream(new FileOutputStream(
+                                recordingFile)));
+                int bufferSize = AudioRecord.getMinBufferSize(frequency,
+                        channelConfiguration, audioEncoding);
+                AudioRecord audioRecord = new AudioRecord(
+                        MediaRecorder.AudioSource.MIC, frequency,
+                        channelConfiguration, audioEncoding, bufferSize);
 
-            int minBufferSize = AudioRecord.getMinBufferSize(11025,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT);
-
-            short[] audioData = new short[minBufferSize];
-
-            AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                    11025,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    minBufferSize);
-
-            audioRecord.startRecording();
-
-            while(recording){
-                int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
-                for(int i = 0; i < numberOfShort; i++){
-                    dataOutputStream.writeShort(audioData[i]);
+                byte[] buffer = new byte[bufferSize];
+                audioRecord.startRecording();
+                int r = 0;
+                while (recording) {
+                    int bufferReadResult = audioRecord.read(buffer, 0,
+                            bufferSize);
+                    for (int i = 0; i < bufferReadResult; i++) {
+                        dos.writeByte(buffer[i]);
+                    }
+                    publishProgress(new Integer(r));
+                    r++;
                 }
+                audioRecord.stop();
+                dos.close();
+            } catch (Throwable t) {
+                Log.e("AudioRecord", "Recording Failed");
             }
-
-            audioRecord.stop();
-            dataOutputStream.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            return null;
         }
+        protected void onProgressUpdate(Integer... progress) {
+        }
+        protected void onPostExecute(Void result) {
+        }
+    }
 
+    private class PlayAudio extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            int bufferSize = AudioTrack.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+            byte[] audiodata = new byte[bufferSize];
+
+            try {
+                DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(recordingFile)));
+                AudioTrack audioTrack = new AudioTrack(
+                        AudioManager.STREAM_MUSIC, frequency,
+                        channelConfiguration, audioEncoding, bufferSize,
+                        AudioTrack.MODE_STREAM);
+
+                audioTrack.play();
+
+                while ( dis.available() > 0) {
+                    int i = 0;
+                    while (dis.available() > 0 && i < audiodata.length) {
+                        audiodata[i] = dis.readByte();
+                        i++;
+                    }
+                    audioTrack.write(audiodata, 0, audiodata.length);
+                }
+                dis.close();
+
+            } catch (Throwable t) {
+                Log.e("AudioTrack", "Playback Failed");
+            }
+            return null;
+        }
     }
 }
